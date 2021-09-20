1ac3bd6026b9f96ebf8985199f21d25189c2d95c
Troy Snowden <troybradsnowden@gmail.com>
2021-09-20 16:11:17 +0100

    Add passing test for docking station class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
+  it 'docking station responds to release_bike method' do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
