862f1be52a7a2490a491388d6dc62d63ef86ad33
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-09-20 17:00:41 +0100

    Added docking bike method, attribute reader and appropriate unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,4 +17,12 @@ describe DockingStation do
     expect(@docking_station.release_bike).to eq(@new_bike)
     expect(@new_bike.working?).to eq(true)
   end
+
+  it 'should allow bikes to be docked in the docking station' do
+    expect(@docking_station.dock_bike(@new_bike)).to eq("Your bike has been stored")
+  end
+
+  it 'should test attr_reader and return instance of the bike' do
+    expect(@docking_station.bike.is_a? Bike).to eq(true)
+  end
 end
