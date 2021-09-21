1933f4561e14187a420a2ba541fb9724cd122163
Troy Snowden <troybradsnowden@gmail.com>
2021-09-21 15:13:06 +0100

    Add capacity of 20 as an instance variable to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,29 @@
 class DockingStation
 
-  def initialize
-    @docked_bikes_total = 3
+  def initialize(bike)
+    @docked_bikes = []
+    @bike = bike
+    @docked_bikes_total = @docked_bikes.length
+    @capacity = 20
   end
 
   def release_bike
     if @docked_bikes_total > 0
       @docked_bikes_total -= 1
-      Bike.new
+      bike = @docked_bikes.pop
+      bike
     else
       raise "There are no bikes to release"
+    end
+  end
+
+  def dock_bike(bike)
+    if @docked_bikes_total < @capacity
+      @docked_bikes_total += 1
+      @docked_bikes.push(bike)
+      bike
+    else
+      raise "There is no room for this bike"
     end
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation do
 
   before(:each) do 
     @new_bike = Bike.new
-    @docking_station = DockingStation.new
+    @docking_station = DockingStation.new(@new_bike)
   end
 
   it 'docking station responds to release_bike method' do
@@ -14,15 +14,20 @@ describe DockingStation do
   end
 
   it 'docking station releases bike' do
+    @docking_station.dock_bike(Bike.new)
     result = @docking_station.release_bike
     expect(result.is_a? Bike).to eq(true)
     expect(@new_bike.working?).to eq(true)
   end
 
   it 'docking station should not release a bike if there are none available' do
-    @docking_station.release_bike
-    @docking_station.release_bike
-    @docking_station.release_bike
     expect{@docking_station.release_bike}.to raise_error("There are no bikes to release")
+  end
+
+  it 'docking station should not add a bike if it is at capacity' do
+    20.times do
+      @docking_station.dock_bike(Bike.new)
+    end
+    expect{@docking_station.dock_bike(@new_bike)}.to raise_error("There is no room for this bike")
   end
 end
